9e56f11f5808ea4901298f4505764b156f0279ca
Samuel <Samusplay@iMac-de-Angela.local>
2026-01-01 16:36:24 -0500

    part1 configuracion auth

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,18 +1,32 @@
 import User from "../models/User"
 import {Request,Response} from 'express'
+import { hashPassword } from "../utils/auth"
 //Tener cuidado con Any
  export const createAccount=async(req:Request,res:Response)=>{
     console.log('Desde Register')
-    //soporta el ingreso de datos
-    console.log(req.body)
-    //formulario tipo post
 
+    //Validaciones
+    const {email,password}=req.body
+
+    //Metodo de moongose para buscr y pasamos objeto
+    const UserExists=await User.findOne({email})
+    //Validaciones
+    if(UserExists){
+        const error=new Error('El usuario ya esta registrado')
+        //debe detener la respuesta conn el objeto y parar
+         return res.status(409).json({error:error.message})
+    }
+    
     //para guardar en la base de datos
     const user=new User(req.body)
+    //la funcion de Utils es asincrona
+    user.password= await hashPassword(password)
+    
+   
 
     await user.save()
     
     //configurar respuesta hasta ahi llega el codigo
-    res.send('Registro Creado Correctamente ')
+    res.status(201).send('Registro Creado Correctamente ')
 
 }
