58e909473bcba6cd9e9c3c4373f5ee272558e51e
samusplay <samusplay08@gmail.com>
2026-01-24 11:17:00 -0500

    agregado cloudinary

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,9 +1,13 @@
 import { Request, Response } from "express";
 import { validationResult } from "express-validator";
 import slug from 'slug';
+//Para subir archivos con express usar formidable
+import formidable from 'formidable'
+import {v4 as uuid}from 'uuid'
 import User from "../models/User";
 import { checkPassword, hashPassword } from "../utils/auth";
 import { generateJWT } from "../utils/jwt";
+import cloudinary from "../config/cloudinary";
 
 //Tener cuidado con Any
 export const createAccount = async (req: Request, res: Response) => {
@@ -96,18 +100,45 @@ export const updateProfile = async (req: Request, res: Response) => {
     const handle = slug(req.body.handle, '-')
     const handleExists = await User.findOne({ handle })
     //Condicion para que el usuario actualize si es el mismo
-    if (handleExists &&handleExists.email!==req.user.email) {
+    if (handleExists && handleExists.email !== req.user.email) {
       const error = new Error('Nombre de usuario no disponible')
       return res.status(409).json({ error: error.message })
     }
     //Actualizar el usuario
-    req.user.description=description
-    req.user.handle=handle
+    req.user.description = description
+    req.user.handle = handle
     //Guardamos
     await req.user.save()
     res.send('Perfil actualizado Correctamente')
 
 
+  } catch (e) {
+    const error = new Error('Hubo un error')
+    return res.status(500).json({ error: error.message })
+  }
+}
+
+export const uploadImage = async (req: Request, res: Response) => {
+  const form = formidable({
+    //subir una foto
+    multiples: false
+  })
+
+
+  try {
+    //foto que el usuario ya subio
+    form.parse(req, (error, fields, files) => {
+
+      cloudinary.uploader.upload(files.file[0].filepath, {public_id:uuid()}, async function (error, result) {
+        if (error) {
+          const error = new Error('Hubo un error al subir la imagen')
+          return res.status(500).json({ error: error.message })
+        }
+        if(result){
+          console.log(result.secure_url)
+        }
+      })
+    })
   } catch (e) {
     const error = new Error('Hubo un error')
     return res.status(500).json({ error: error.message })
